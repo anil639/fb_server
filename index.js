7506e6bdf3fd7607d9f2fdb5bd182ce1d4cecb8f
Anil kumar nayak <anilkumarnayak639@gmail.com>
2024-02-26 11:58:14 +0530

    setUp updated

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// configuration
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+// file Storage
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+//db Setup
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection established"))
+  .catch((e) => console.log(e));
+app.use(express.json());
+const port = process.env.PORT || 8001;
+app.get("/", (req, res) => {
+  res.send("Anil kumar nayak");
+});
+app.listen(port, () => console.log(`app listening on port ${port}`));
